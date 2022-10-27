b8aeae0261545472a04b28d86e49ed489f243469
Jessica <jsouzasouza120@gmail.com>
2022-10-26 22:16:14 -0300

    Inclusão da classe Cd

---
+++ src/Cd.java
@@ -0,0 +1,30 @@
+public class Cd extends Midia {
+    private String artista;
+    private int quantidadeFaixas;
+
+    public void darPlay() {
+        System.out.println("Primeira faixa tocando");
+    }
+
+    public Cd(String codigo, Double preco, String nome, String artista, int quantidadeFaixas) {
+        super(codigo, preco, nome);
+        this.artista = artista;
+        this.quantidadeFaixas = quantidadeFaixas;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public int getQuantidadeFaixas() {
+        return quantidadeFaixas;
+    }
+
+    public void setQuantidadeFaixas(int quantidadeFaixas) {
+        this.quantidadeFaixas = quantidadeFaixas;
+    }
+}
